59589ecad03cbfb94a62ee8ee7577ab26e6816f2
Imalli5253 <pnr.759@gmail.com>
2019-07-19 15:11:29 +0530

    Git Trigger

--- one.java
+++ one.java
@@ -4,6 +4,8 @@ class One
 	System.out.println("This is Java Program");
 	System.out.println("First Name : "+args[0]);
 	System.out.println("Last Name : "+args[1]);
-
+	int n=10;
+	for(int i=1;i<=n;i++)
+		System.out.println(i+" * "+n+" = "+(i*n));
 	}
 }
